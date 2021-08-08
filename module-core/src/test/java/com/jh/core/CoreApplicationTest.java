7b9fc4504d1912111524e60f91af6182ad89cc41
jamez <jamez@gccompany.co.kr>
2021-08-08 18:08:06 +0900

    initial commit

---
+++ module-core/src/test/java/com/jh/core/CoreApplicationTest.java
@@ -0,0 +1,8 @@
+package com.jh.core;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplicationTest {
+    public void contextLoads() {}
+}
